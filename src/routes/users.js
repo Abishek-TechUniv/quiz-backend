00515d787619b9f7556ea3254eafc202e3fdb464
Abishek Aditya <abishekaditya@outlook.com>
2018-02-28 01:16:10 +0530

    Added route for response

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,4 @@
-const { users } = require('../../models');
+const { users, responses } = require('../../models');
 const { calculate } = require('../helpers');
 
 module.exports = [
@@ -11,6 +11,24 @@ module.exports = [
         { userName: request.payload.userName },
       })
         .then(() => reply('User in database').code(200));
+    },
+  },
+  {
+    method: 'POST',
+    path: '/users/response',
+    handler: (request, reply) => {
+      responses.findAll({
+        where:
+        { userName: request.payload.userName },
+      })
+        .then((result) => {
+          const output = [];
+          result.forEach((element) => {
+            const { questionId, response } = element;
+            output.push({ questionId, response });
+          });
+          reply(output).code(200);
+        });
     },
   },
   {
@@ -38,6 +56,15 @@ module.exports = [
             ({ userName: result.userName, score: result.score }));
           reply(resultOutput).code(200);
         });
+    },
+  },
+  {
+    method: 'POST',
+    path: '/score',
+    handler: (request, reply) => {
+      const { userName } = request.payload;
+      users.findOne({ where: { userName } })
+        .then(({ score }) => reply(score));
     },
   },
 ];
